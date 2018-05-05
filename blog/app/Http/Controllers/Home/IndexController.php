391dc532ae7d7df4732663086338ea12bf4f4c98
“用maohuiru <1600951876@qq.com>
2018-05-05 10:00:20 +0800

    Signed-off-by: “用maohuiru <“1600951876@qq.com> 框架

---
+++ blog/app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class IndexController extends Controller
+{
+
+    public function add()
+    {
+        echo "11111";
+    }
+}
